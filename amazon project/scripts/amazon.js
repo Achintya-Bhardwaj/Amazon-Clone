7c724ed6b1cbc3072e22c228039bf87dc6f92667
Achintya <achib2003@gmail.com>
2024-01-10 16:12:44 +0530

    add to cart button javascript

--- amazon project/scripts/amazon.js
+++ amazon project/scripts/amazon.js
@@ -1,6 +1,7 @@
 const productGrid = document.querySelector('.products-grid');
 
 
+
 let htmlAccumulator = '';
 products.forEach((product) => {
   htmlAccumulator += `
@@ -48,11 +49,38 @@ products.forEach((product) => {
       Added
     </div>
 
-    <button class="add-to-cart-button button-primary">
+    <button class="add-to-cart-button button-primary "
+      data-product-id="${product.id}">
       Add to Cart
     </button>
   </div>
   `
 });
-
 productGrid.innerHTML = htmlAccumulator;
+
+const addToCartBut = document.querySelectorAll('.add-to-cart-button');
+
+addToCartBut.forEach((button) => {
+  button.addEventListener('click', () => {
+    const productId = button.dataset.productId;
+    
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if(productId === item.productId){
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId,
+        quantity: 1
+      });
+    }
+
+    console.log(cart);
+  });
+});
